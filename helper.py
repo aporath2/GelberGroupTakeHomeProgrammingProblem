11e6fb6514cfe4872e158b235e21fc953ed9ad8a
Avi Poarth <aviporath@gmail.com>
2021-04-06 01:09:35 -0500

    first draft, needs sorting and testing

---
+++ helper.py
@@ -0,0 +1,53 @@
+def  fetchInputFile():
+    #Opens the input file
+    f = open("input.txt", "r")
+
+    #Reads the first line to learn the number of registers
+    numRegisters = int(f.readline())
+
+    #Reads the second line to learn of the first customer
+    customer = f.readline()
+
+    #Creates a list of customers
+    customers = []
+
+    #Continually reads for new customers until it reachs the end of the input file
+    while customer:
+        customers.append(customer.split())
+        customer = f.readline()
+
+    #Returns a tuple that contains the number of registers and the list of customers
+    return(numRegisters,customers)
+
+
+def newCustomerEnters(customer, registers):
+
+    #Logic for a type A customer
+    if customer[0] == 'A':
+
+        #Finds the register with the fewest people in line
+        numRegister = registers.index(min(registers, key = len))
+
+        #Gets the number of items the new customer has
+        numItems = int(customer[2])
+
+        #Returns the register the customer picks and the number of items they have
+        return (numRegister, numItems)
+
+    #Logic for a type B customer
+    if customer[0] == 'B':
+
+        #Creates a list of the number of items held by the last person in each line
+        lastPersonItems = [x[-1] if x else 0 for x in registers]
+
+        #Finds the register with the fewest items held by the last person in each line
+        numRegister = lastPersonItems.index(min(lastPersonItems))
+
+        #Gets the number of items the new customer has
+        numItems = int(customer[2])
+
+        #Returns the register the customer picks and the number of items they have
+        return (numRegister, numItems)
+
+def sortCustomers():
+    pass
---
+++ sim.py
@@ -0,0 +1,72 @@
+from helper import *
+
+#Reads through the input file and sets the number of registers and a list of customers
+numRegisters, customers = fetchInputFile()
+
+#SORT CUSTOMERS
+
+#Creates a list of lists representing the different registers at checkout
+registers = [[] for _ in range(numRegisters)]
+
+#Sets the starting time to zero
+time = 0
+
+#Creates a training flag to allow the new hire double time
+trainingFlag = False
+
+#Simulation runs while there are any potential customers or anyone checking out
+while customers or any(registers):
+
+    #Checks if there is a new customer approaching checkout at the current time
+    if customers and int(customers[0][1]) == time:
+
+        #Gets the register the customer will pick and they number of items they have
+        numRegister, numItems = newCustomerEnters(customers.pop(0), registers)
+
+        #Adds that customer and there item to the register that they picked
+        registers[numRegister].extend([numItems])
+
+        #Starts a new iteration in case there are more new customers at the same time
+        continue
+
+    #Iterates through every register but the training one
+    for i in range(0, numRegisters-1):
+
+        #Checks if there is at least one person in line
+        if registers[i]:
+
+            #Checks out one item
+            registers[i][0] -= 1
+
+            #Checks if the customer is out of items
+            if registers[i][0] == 0:
+
+                #Removes the customer from the line
+                registers[i].pop(0)
+
+    #Checks there is at least one person in line at the training register
+    if registers[-1]:
+
+        #Checks if the cashier in training checked out some item last time interval
+        if trainingFlag == True:
+
+            #Checks out one item
+            registers[-1][0] -= 1
+
+            #Checks if the customer is out of items
+            if registers[-1][0] == 0:
+
+                #Removes the customer from the line
+                registers[-1].pop(0)
+
+            #inverts the trainingFlag so they do not checkout an item next time interval
+            trainingFlag = False
+
+        #Otherwise inverts the trainingFlag so they checkout an item next time interval
+        else:
+            trainingFlag = True
+    #Updates the time interval
+    time += 1
+
+#Prints out the final time
+print("Finished at: t=" + str(time))
